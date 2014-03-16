456a9118bf2f486a4ab8bbe18bb928a768a3388e
Michael Milette <michael.milette@instruxmedia.com>
2014-03-16 17:42:19 -0400

    MDL-43587-M25 - User Profile - Custom menu field data should not be processed through language filter when saving
    Affected files:
    
      /user/profile/field/menu/field.class.php
    
    Signed-off-by: Michael Milette <michael.milette@instruxmedia.com>

--- user/profile/field/menu/field.class.php
+++ user/profile/field/menu/field.class.php
@@ -55,7 +55,7 @@ class profile_field_menu extends profile_field_base {
             $this->options[''] = get_string('choose').'...';
         }
         foreach ($options as $key => $option) {
-            $this->options[$key] = format_string($option); // Multilang formatting.
+            $this->options[$key] = $option;
         }
 
         // Set the data key.
@@ -70,6 +70,9 @@ class profile_field_menu extends profile_field_base {
      * @param moodleform $mform Moodle form instance
      */
     public function edit_field_add($mform) {
+        foreach ($this->options as $key => $option) {
+            $this->options[$key] = format_string($option);
+        }
         $mform->addElement('select', $this->inputname, format_string($this->field->name), $this->options);
     }
 
@@ -79,6 +82,7 @@ class profile_field_menu extends profile_field_base {
      * @param moodleform $mform Moodle form instance
      */
     public function edit_field_set_default($mform) {
+        $this->field->defaultdata = format_string($this->field->defaultdata);
         if (false !== array_search($this->field->defaultdata, $this->options)) {
             $defaultkey = (int)array_search($this->field->defaultdata, $this->options);
         } else {
@@ -143,5 +147,3 @@ class profile_field_menu extends profile_field_base {
         return $retval;
     }
 }
-
-
