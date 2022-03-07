0e92b41c00b329fec50bf13195c2d2aa9b518c88
Michael Milette <michael.milette@tngconsulting.ca>
2022-03-07 00:03:39 -0500

    MDL-63219 Output: Enable filters in custom menus.

--- admin/tool/mobile/classes/api.php
+++ admin/tool/mobile/classes/api.php
@@ -33,6 +33,7 @@ use lang_string;
 use curl;
 use core_qrcode;
 use stdClass;
+use filter_manager;
 
 /**
  * API exposed by tool_mobile, to be used mostly by external functions and the plugin settings.
@@ -320,7 +321,14 @@ class api {
             $settings->tool_mobile_customlangstrings = get_config('tool_mobile', 'customlangstrings');
             $settings->tool_mobile_disabledfeatures = get_config('tool_mobile', 'disabledfeatures');
             $settings->tool_mobile_filetypeexclusionlist = get_config('tool_mobile', 'filetypeexclusionlist');
-            $settings->tool_mobile_custommenuitems = get_config('tool_mobile', 'custommenuitems');
+            $custommenuitems = get_config('tool_mobile', 'custommenuitems');
+            // If filtering of the primary custom menu is enabled, apply only the string filters.
+            if (!empty($CFG->navfilter) && !empty($CFG->stringfilters)) {
+                // Apply filters that are enabled for Content and Headings.
+                $filtermanager = \filter_manager::instance();
+                $custommenuitems = $filtermanager->filter_string($custommenuitems, \context_system::instance());
+            }
+            $settings->tool_mobile_custommenuitems = $custommenuitems;
             $settings->tool_mobile_apppolicy = get_config('tool_mobile', 'apppolicy');
             // This setting could be not set in some edge cases such as bad upgrade.
             $mintimereq = get_config('tool_mobile', 'autologinmintimebetweenreq');
