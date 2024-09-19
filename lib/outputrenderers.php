b6cd16d40e4de1ab4d0a2a12d088f73af5ed3a0a
Michael Milette <michael.milette@tngconsulting.ca>
2024-09-19 00:13:40 -0400

    MDL-63219 Output: Enable filters in custom menus.

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -3771,7 +3771,7 @@ EOD;
     }
 
     /**
-     * Returns the custom menu if one has been set
+     * Applies Moodle filters to the custom menu and returns the custom menu if one has been set.
      *
      * A custom menu can be configured by browsing to
      *    Settings: Administration > Appearance > Themes > Theme settings
@@ -3780,8 +3780,8 @@ EOD;
      * Theme developers: DO NOT OVERRIDE! Please override function
      * {@link core_renderer::render_custom_menu()} instead.
      *
-     * @param string $custommenuitems - custom menuitems set by theme instead of global theme settings
-     * @return string
+     * @param string $custommenuitems - custom menuitems set by theme instead of global theme settings.
+     * @return string Rendered custom_menu after filters have been applied.
      */
     public function custom_menu($custommenuitems = '') {
         global $CFG;
@@ -3789,6 +3789,14 @@ EOD;
         if (empty($custommenuitems) && !empty($CFG->custommenuitems)) {
             $custommenuitems = $CFG->custommenuitems;
         }
+
+        // If filtering of the primary custom menu is enabled, apply only the string filters.
+        if (!empty($CFG->navfilter) && !empty($CFG->stringfilters)) {
+            // Apply filters that are enabled for Content and Headings.
+            $filtermanager = filter_manager::instance();
+            $custommenuitems = $filtermanager->filter_string($custommenuitems, \context_system::instance());
+        }
+
         $custommenu = new custom_menu($custommenuitems, current_language());
         return $this->render_custom_menu($custommenu);
     }
@@ -3804,6 +3812,14 @@ EOD;
         if (empty($custommenuitems) && !empty($CFG->custommenuitems)) {
             $custommenuitems = $CFG->custommenuitems;
         }
+
+        // If filtering of the primary custom menu is enabled, apply only the string filters.
+        if (!empty($CFG->navfilter) && !empty($CFG->stringfilters)) {
+            // Apply filters that are enabled for Content and Headings.
+            $filtermanager = filter_manager::instance();
+            $custommenuitems = $filtermanager->filter_string($custommenuitems, \context_system::instance());
+        }
+
         $custommenu = new custom_menu($custommenuitems, current_language());
         $langs = get_string_manager()->get_list_of_translations();
         $haslangmenu = $this->lang_menu() != '';
